9ee3232eb394c1360d9df3af790d24e474cb4e54
frakhdz <robbengiant75@gmail.com>
2022-03-16 16:56:04 -0500

    added left moves same glitch

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+//array containing all checker pieces to track movement
+//every piece has a different int value
 board = [
     null, 0,    null, 1,   null, 2,     null, 3,
     4,    null, 5,    null, 6,   null,  7,    null,
@@ -10,31 +11,42 @@ board = [
     20,   null, 21,   null, 22,   null, 23,   null
 ]
 
+//tracks how much red pieces are in the board
 let redsPieces = document.querySelectorAll("p");
+
+//determines the size of the board
 let cells = document.querySelectorAll("td");
 
+//checks if any red piece is clicked calls clicked by passing i as id
 for (let i = 0; i < redsPieces.length; i++){
-//redsPieces[10].addEventListener("click", prtnr(10));
-redsPieces[i].addEventListener("click", prtnr.bind(event, i));
+    redsPieces[i].addEventListener("click", clicked.bind(event, i));
 }
 
-function prtnr(x) {
+//used when we click a red piece
+function clicked(id) {
     //window.alert("click red");
-    let indexBoard = findIndex(x);
-    cells[indexBoard+9].setAttribute("onclick", "move(" + x + ")");
-    //cells[indexBoard+7].addEventListener("click", prtnr2);
-    //cells[indexBoard+7].addEventListener("click", right);
+
+    //finds where piece with id value is on the board
+    let indexBoard = findIndex(id);
+    
+    //move to the right when we click the a valid move to the right side
+    //call move(id) to move
+    cells[indexBoard+9].setAttribute("onclick", "move(" + id + "," + 9 + ")");
+    cells[indexBoard+7].setAttribute("onclick", "move(" + id + "," + 7 +")");
+
 }
 
-function move(mv){
+function move(mv, dir){
+
     let indexBoard = findIndex(mv);
     //document.getElementById(mv).remove();
     
     //cells[mv].innerHTML = "";
     cells[indexBoard].innerHTML = "";
-
-    cells[indexBoard + 9].innerHTML = `<p class="red-piece" id="${mv}"></p>`;
-    moveBoard(mv, indexBoard + 9);
+    
+        cells[indexBoard + dir].innerHTML = `<p class="red-piece" id="${mv}"></p>`;
+        moveBoard(mv, indexBoard + dir);
+    
     redsPieces = document.querySelectorAll("p");
 
 
@@ -50,23 +62,6 @@ function moveBoard(start, end){
 
 }
 
-function right() {
-    
-    if(board[9+9] == null){
-        window.alert("move right");
-
-    }
-
-}
-
-function prtnr2() {
-    
-    if(board[9+7] == null){
-        window.alert("move left");
-
-    }
-
-}
 function findIndex(indx) {
     for (let i = 0; i < board.length; i++){
         
